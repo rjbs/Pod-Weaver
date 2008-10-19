c7730618c6b57f6fcb6dde1d43d0affe4312d3ee
Ricardo SIGNES <rjbs@cpan.org>
2008-10-18 23:07:43 -0400

    steal from dzil

---
+++ lib/Pod/Weaver/Role/Plugin.pm
@@ -0,0 +1,21 @@
+package Pod::Weaver::Role::Plugin;
+
+use Moose::Role;
+
+has plugin_name => (
+  is  => 'ro',
+  isa => 'Str',
+  required => 1,
+  init_arg => '=name',
+);
+
+has weaver => (
+  is  => 'ro',
+  isa => 'Pod::Weaver',
+  required => 1,
+  weak_ref => 1,
+  handles  => [ qw(log) ],
+);
+
+no Moose::Role;
+1;
