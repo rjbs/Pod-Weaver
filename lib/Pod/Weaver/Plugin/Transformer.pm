ab3f112b81c5ef6753ad4bdc2043f6e3960b8d6e
Ricardo Signes <rjbs@cpan.org>
2009-12-19 22:49:29 -0500

    the Transformer dialect works

--- lib/Pod/Weaver/Plugin/Transformer.pm
+++ lib/Pod/Weaver/Plugin/Transformer.pm
@@ -11,6 +11,22 @@ use String::RewritePrefix;
 
 =head1 OVERVIEW
 
+This plugin acts as a L<Pod::Weaver::Role::Dialect> that applys an arbitrary
+L<Pod::Elemental::Transformer> to your input document.  It is configured like
+this:
+
+  [Transformer / Lists]
+  transformer = List
+  format_name = outline
+
+This will end up creating a transformer like this:
+
+  my $xform = Pod::Elemental::Transformer::List->new({
+    format_name => 'outline',
+  });
+
+and that transformer will then be handed the entire input Pod document.
+
 =cut
 
 has transformer => (is => 'ro', required => 1);
@@ -26,7 +42,7 @@ sub BUILDARGS {
 
   my $xform_class = String::RewritePrefix->rewrite(
     { '' => 'Pod::Elemental::Transformer::', '=' => '' },
-    delete $class_args{transformer},
+    delete $xform_args{transformer},
   );
 
   Class::MOP::load_class($xform_class);
@@ -51,87 +67,3 @@ sub translate_dialect {
 }
 
 1;
-
-__END__
-=pod
-
-=head1 NAME
-
-Pod::Weaver::Plugin::WikiDoc - allow wikidoc-format regions to be translated during dialect phase
-
-=head1 VERSION
-
-version 0.093001
-
-=head1 OVERVIEW
-
-This plugin is an exceedingly thin wrapper around
-L<Pod::Elemental::Transformer::WikiDoc>.  When you load this plugin, then
-C<=begin> and C<=for> regions with the C<wikidoc> format will be translated to
-standard Pod5 before further weaving continues.
-
-=head1 AUTHOR
-
-  Ricardo SIGNES <rjbs@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2009 by Ricardo SIGNES.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
-
-package Pod::Weaver::Plugin::WikiDoc;
-our $VERSION = '0.093001';
-
-
-use Moose;
-with 'Pod::Weaver::Role::Dialect';
-# ABSTRACT: allow wikidoc-format regions to be translated during dialect phase
-
-
-use namespace::autoclean;
-
-use Pod::Elemental::Transformer::WikiDoc;
-
-sub translate_dialect {
-  my ($self, $pod_document) = @_;
-
-  Pod::Elemental::Transformer::WikiDoc->new->transform_node($pod_document);
-}
-
-1;
-
-__END__
-=pod
-
-=head1 NAME
-
-Pod::Weaver::Plugin::WikiDoc - allow wikidoc-format regions to be translated during dialect phase
-
-=head1 VERSION
-
-version 0.093001
-
-=head1 OVERVIEW
-
-This plugin is an exceedingly thin wrapper around
-L<Pod::Elemental::Transformer::WikiDoc>.  When you load this plugin, then
-C<=begin> and C<=for> regions with the C<wikidoc> format will be translated to
-standard Pod5 before further weaving continues.
-
-=head1 AUTHOR
-
-  Ricardo SIGNES <rjbs@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is copyright (c) 2009 by Ricardo SIGNES.
-
-This is free software; you can redistribute it and/or modify it under
-the same terms as the Perl 5 programming language system itself.
-
-=cut
-
