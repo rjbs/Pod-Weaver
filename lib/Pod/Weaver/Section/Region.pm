403680b66b172230093eb1037ad500b986ecff40
Apocalypse <perl@0ne.us>
2010-06-29 16:39:17 -0700

    add allow_nonpod+flatten to Section::Region

--- lib/Pod/Weaver/Section/Region.pm
+++ lib/Pod/Weaver/Section/Region.pm
@@ -23,6 +23,30 @@ has region_name => (
   default  => sub { $_[0]->plugin_name },
 );
 
+=attr allow_nonpod
+
+A boolean value specifying whether nonpod regions are allowed or not. Defaults to false.
+
+=cut
+
+has allow_nonpod => (
+  is  => 'ro',
+  isa => 'Bool',
+  default => 0,
+);
+
+=attr flatten
+
+A boolean value specifying whether the region's contents should be flattened or not. Defaults to true.
+
+=cut
+
+has flatten => (
+  is  => 'ro',
+  isa => 'Bool',
+  default => 1,
+);
+
 sub weave_section {
   my ($self, $document, $input) = @_;
 
@@ -32,10 +56,15 @@ sub weave_section {
   IDX: for (my $i = 0; $i < $idc->length; $i++) {
     next unless my $para = $idc->[ $i ];
     next unless $para->isa('Pod::Elemental::Element::Pod5::Region')
-         and    $para->is_pod
          and    $para->format_name eq $self->region_name;
+    next if     !$self->allow_nonpod and !$para->is_pod;
 
-    push @to_insert, $para->children->flatten;
+    if ( $self->flatten ) {
+      push @to_insert, $para->children->flatten;
+    } else {
+      push @to_insert, $para;
+    }
+
     splice @$idc, $i, 1;
 
     redo IDX;
