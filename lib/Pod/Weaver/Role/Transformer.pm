2f20bd769daffdb1e0a3df88e30fd40a2db2a014
Randy Stauner <randy@magnificent-tears.com>
2012-04-18 13:53:33 -0700

    Fix module reference in Transformer Role pod
    likely a copy-paste oversight

--- lib/Pod/Weaver/Role/Transformer.pm
+++ lib/Pod/Weaver/Role/Transformer.pm
@@ -5,7 +5,7 @@ with 'Pod::Weaver::Role::Plugin';
 
 =head1 IMPLEMENTING
 
-The Preparer role indicates that a plugin will be used to pre-process the input
+The Transformer role indicates that a plugin will be used to pre-process the input
 hashref's Pod document before weaving begins.  The plugin must provide a
 C<transform_document> method which will be called with the input Pod document.
 It is expected to modify the input in place.
