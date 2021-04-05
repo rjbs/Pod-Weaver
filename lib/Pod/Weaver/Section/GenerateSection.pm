8ed825901cf78f49ed6c3c13e1c76496c9f544c2
Ricardo Signes <rjbs@semiotic.systems>
2021-04-05 17:01:05 -0400

    GenerateSection: re-interleave docs

--- lib/Pod/Weaver/Section/GenerateSection.pm
+++ lib/Pod/Weaver/Section/GenerateSection.pm
@@ -15,40 +15,65 @@ use Text::Template;
 
 use namespace::autoclean;
 
-# BEGIN CODE IMPORTED FROM Dist::Zilla::Role::TextTemplate
-has delim => (
-  is   => 'ro',
-  isa  => 'ArrayRef',
-  lazy => 1,
-  init_arg => undef,
-  default  => sub { [ qw(  {{  }}  ) ] },
-);
+=head1 SYNOPSIS
 
-sub fill_in_string {
-  my ($self, $string, $stash, $arg) = @_;
+In your F<weaver.ini>
 
-  $self->log_fatal("Cannot use undef as a template string")
-    unless defined $string;
+  [GenerateSection]
+  title = HOMEPAGE
+  text  = This is the POD for distribution {{$name}}. Check out what we have
+  text  = been up to at {{$homepage}}
 
-  my $tmpl = Text::Template->new(
-    TYPE       => 'STRING',
-    SOURCE     => $string,
-    DELIMITERS => $self->delim,
-    BROKEN     => sub { my %hash = @_; die $hash{error}; },
-    %$arg,
-  );
+The title value can be omited if passed as the plugin name:
 
-  $self->log_fatal("Could not create a Text::Template object from:\n$string")
-    unless $tmpl;
+  [GenerateSection / HOMEPAGE]
 
-  my $content = $tmpl->fill_in(%$arg, HASH => $stash);
+=head1 DESCRIPTION
 
-  $self->log_fatal("Filling in the template returned undef for:\n$string")
-    unless defined $content;
+This plugin allows the creation of simple text sections, with or without the
+use of Text::Template for templated text.
 
-  return $content;
-}
-# END CODE IMPORTED FROM Dist::Zilla::Role::TextTemplate
+The C<text> parameters become the lines of the template.
+
+The values of text are concatenated and variable names with matching values on
+the distribution are interpolated.  Specifying the heading level allows one to
+write down a rather long section of POD text without need for extra files. For
+example:
+
+  [GenerateSection / FEEDBACK]
+  head = 1
+  [GenerateSection / Reporting bugs]
+  head = 2
+  text = Please report bugs when you find them. While we do have a mailing
+  text = list, please use the bug tracker at {{$bugtracker_web}}
+  text = to report bugs
+  [GenerateSection / Homegape]
+  head = 2
+  text = Also, come check out our other projects at
+  text = {{$homepage}}
+
+=head1 TEMPLATE RENDERING
+
+When rendering as a template, the variables C<$plugin>, C<$dist>, and
+C<$distmeta> will be provided, set to the GenerateSection plugin,
+C<Dist::Zilla> object, and the distribution metadata hash respectively. For
+convenience, the following variables are also set:
+
+=for :list
+* C<< $name >>
+* C<< $version >>
+* C<< $homepage >>
+* C<< $repository_web >>
+* C<< $repository_url >>
+* C<< $bugtracker_web >>
+* C<< $bugtracker_email >>
+
+=attr text
+
+The text to be added to the section. Multiple values are allowed and will be
+concatenated. Certain sequences on the text will be replaced (see below).
+
+=cut
 
 sub mvp_multivalue_args { return qw(text) }
 has text => (
@@ -58,12 +83,25 @@ has text => (
   default => sub { [] },
 );
 
+=attr head
+
+This is the I<X> to use in the C<=headX> that's created.  If it's C<0> then no
+heading is added.  It defaults to C<1>.
+
+=cut
+
 has head => (
   is      => 'ro',
   isa     => 'Int',
   lazy    => 1,
   default => 1,
 );
+
+=attr title
+
+The title for this section.  If none is given, the plugin's name is used.
+
+=cut
 
 has title => (
   is      => 'ro',
@@ -72,12 +110,26 @@ has title => (
   default => sub { $_[0]->plugin_name },
 );
 
+=attr main_module_only
+
+If true, this attribute indicates that only the main module's Pod should be
+altered.  By default, it is false.
+
+=cut
+
 has main_module_only => (
   is      => 'ro',
   isa     => 'Bool',
   lazy    => 1,
   default => 0,
 );
+
+=attr
+
+If true, the text is treated as a L<Text::Template> template and rendered.
+This attribute B<is true by default>.
+
+=cut
 
 has is_template => (
   is      => 'ro',
@@ -130,123 +182,52 @@ sub weave_section {
   push @{ $document->children }, $element;
 }
 
+# BEGIN CODE IMPORTED FROM Dist::Zilla::Role::TextTemplate
+=attr delim
+
+If given, this must be an arrayref with two elements.  These will be the
+opening and closing delimiters of template variable sections.  By default they
+are C<{{> and C<}}>.
+
+=cut
+
+has delim => (
+  is   => 'ro',
+  isa  => 'ArrayRef',
+  lazy => 1,
+  init_arg => undef,
+  default  => sub { [ qw(  {{  }}  ) ] },
+);
+
+sub fill_in_string {
+  my ($self, $string, $stash, $arg) = @_;
+
+  $self->log_fatal("Cannot use undef as a template string")
+    unless defined $string;
+
+  my $tmpl = Text::Template->new(
+    TYPE       => 'STRING',
+    SOURCE     => $string,
+    DELIMITERS => $self->delim,
+    BROKEN     => sub { my %hash = @_; die $hash{error}; },
+    %$arg,
+  );
+
+  $self->log_fatal("Could not create a Text::Template object from:\n$string")
+    unless $tmpl;
+
+  my $content = $tmpl->fill_in(%$arg, HASH => $stash);
+
+  $self->log_fatal("Filling in the template returned undef for:\n$string")
+    unless defined $content;
+
+  return $content;
+}
+# END CODE IMPORTED FROM Dist::Zilla::Role::TextTemplate
+
 __PACKAGE__->meta->make_immutable;
 1;
-
-__END__
-
-=pod
-
-=encoding UTF-8
-
-=head1 NAME
-
-Pod::Weaver::Section::GenerateSection - add pod section from an interpolated piece of text
-
-=head1 VERSION
-
-version 1.06
-
-=head1 SYNOPSIS
-
-In your F<weaver.ini>
-
-  [GenerateSection]
-  title = HOMEPAGE
-  text  = This is the POD for distribution {{$name}}. Check out what we have
-  text  = been up to at {{$homepage}}
-
-The title value can be omited if passed as the plugin name:
-
-  [GenerateSection / HOMEPAGE]
-
-=head1 DESCRIPTION
-
-This plugin attempts to be a cross between L<Pod::Weaver::Section::Template> and
-L<Dist::Zilla::Plugin::GenerateFile> without the generation of extra files.
-
-The values of text are concatenated and variable names with matching values on
-the distribution are interpolated. Specifying the heading level allows one to
-write down a rather long section of POD text without need for extra files. For
-example:
-
-  [GenerateSection / FEEDBACK]
-  head = 1
-  [GenerateSection / Reporting bugs]
-  head = 2
-  text = Please report bugs when you find them. While we do have a mailing
-  text = list, please use the bug tracker at {{$bugtracker_web}}
-  text = to report bugs
-  [GenerateSection / Homegape]
-  head = 2
-  text = Also, come check out our other projects at
-  text = {{$homepage}}
-
-=head1 ATTRIBUTES
-
-=head2 text
-
-The text to be added to the section. Multiple values are allowed and will be
-concatenated. Certain sequences on the text will be replaced (see below).
-
-=head2 head
-
-The heading level of this section. If 0, there will be no heading. Defaults to 1.
-
-=head2 title
-
-The title for this section. It can optionally be omitted and passed as the
-plugin name.
-
-=head2 main_module_only
-
-If true, it will add the text only to the main module POD. Defaults to false.
-
-=head2 is_template
-
-If false, it will not attempt to replace the {{}} entries on text. Defaults to
-true.
-
-=for Pod::Coverage mvp_multivalue_args
-
-=head1 Text as template
-
-Unless the option C<is_template> is false, the text will be run through
-L<Text::Template>.  The variables C<$plugin>, C<$dist>, and C<$distmeta> will be
-provided, set to the GenerateSection plugin, C<Dist::Zilla> object, and the
-distribution metadata hash respectively. For convenience, the following
-variables are also set:
-
-=over
-
-=item $name
-
-=item $version
-
-=item $homepage
-
-=item $repository_web
-
-=item $repository_url
-
-=item $bugtracker_web
-
-=item $bugtracker_email
-
-=back
-
-=for Pod::Coverage weave_section
 
 =head1 AUTHOR
 
 Carnë Draug <cdraug@cpan.org>
-
-=head1 COPYRIGHT AND LICENSE
-
-This software is Copyright (c) 2013-2017 by Carnë Draug.
-
-This is free software, licensed under:
-
-  The GNU General Public License, Version 3, June 2007
-
-=cut
